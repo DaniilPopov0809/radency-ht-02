73a300edf05e9840a1e03b4e6d498a55b45d474f
Daniil Popov <x6uhrox@gmail.com>
2023-08-06 19:56:32 +0300

    ADDED: header, footer, logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,140 @@
+const Logo = () => {
+  return (
+    <svg
+      xmlSpace="preserve"
+      viewBox="0 0 100 100"
+      y={0}
+      x={0}
+      xmlns="http://www.w3.org/2000/svg"
+      id="Layer_1"
+      version="1.1"
+      width="40px"
+     
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      style={{ width: '100%', height: '100%' }}
+    >
+      <g className="ldl-scale" style={{ transformOrigin: '50% 50%', transform: 'rotate(0deg) scale(1, 1)', animationPlayState: 'paused' }}>
+        <path
+          d="M9 16.553v66.894H59.877V16.553z"
+          strokeMiterlimit={10}
+          strokeWidth="3.5"
+          strokeLinejoin="round"
+          stroke="#333"
+          fill="#fff"
+          style={{ stroke: 'rgb(51, 51, 51)', fill: 'rgb(255, 255, 255)', animationPlayState: 'paused' }}
+        />
+        <path
+          d="M20.306 26.917h28.265"
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeWidth="3.5"
+          stroke="#333"
+          strokeLinejoin="round"
+          fill="none"
+          style={{ stroke: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+        />
+        <path
+          d="M20.306 36.339h28.265"
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeWidth="3.5"
+          stroke="#333"
+          strokeLinejoin="round"
+          fill="none"
+          style={{ stroke: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+        />
+        <path
+          d="M20.306 45.76h28.265"
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeWidth="3.5"
+          stroke="#333"
+          strokeLinejoin="round"
+          fill="none"
+          style={{ stroke: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+        />
+        <path
+          d="M20.306 55.182h28.265"
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeWidth="3.5"
+          stroke="#333"
+          strokeLinejoin="round"
+          fill="none"
+          style={{ stroke: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+        />
+        <path
+          d="M20.306 64.604h28.265"
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeWidth="3.5"
+          stroke="#333"
+          strokeLinejoin="round"
+          fill="none"
+          style={{ stroke: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+        />
+        <path
+          d="M20.306 74.025h4.854"
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeWidth="3.5"
+          stroke="#333"
+          strokeLinejoin="round"
+          fill="none"
+          style={{ stroke: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+        />
+        <g style={{ animationPlayState: 'paused' }}>
+          <path
+            d="M71.68 24.985l11.992 11.991-33.31 33.31L38.37 58.295z"
+            strokeMiterlimit={10}
+            strokeWidth="3.499965"
+            fill="#f47e5f"
+            strokeLinejoin="round"
+            stroke="#333"
+            style={{ stroke: 'rgb(51, 51, 51)', fill: 'rgb(244, 126, 95)', animationPlayState: 'paused' }}
+          />
+          <path
+            d="M79.008 17.656l11.991 11.992-7.328 7.328-11.992-11.992z"
+            strokeMiterlimit={10}
+            strokeWidth="3.499965"
+            fill="#f4e6c8"
+            strokeLinejoin="round"
+            stroke="#333"
+            style={{ stroke: 'rgb(51, 51, 51)', fill: 'rgb(244, 230, 200)', animationPlayState: 'paused' }}
+          />
+          <path
+            strokeLinejoin="round"
+            stroke="#333"
+            fill="#f7b26a"
+            strokeMiterlimit={10}
+            strokeWidth="3.5"
+            d="M50.098 70.551L38.106 58.559l-4.289 15.319a.78.78 0 0 0 .962.962l15.319-4.289z"
+            style={{ stroke: 'rgb(51, 51, 51)', fill: 'rgb(247, 178, 106)', animationPlayState: 'paused' }}
+          />
+          <path
+            d="M51.354 57.956L71.341 37.97"
+            strokeMiterlimit={10}
+            strokeLinecap="round"
+            strokeWidth="3.5"
+            stroke="#333"
+            strokeLinejoin="round"
+            fill="none"
+            style={{ stroke: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+          />
+          <path
+            strokeLinejoin="round"
+            stroke="#333"
+            strokeLinecap="round"
+            fill="#333"
+            strokeMiterlimit={10}
+            strokeWidth="3.5"
+            d="M35.419 68.156l-1.602 5.722a.78.78 0 0 0 .962.962l5.722-1.602-5.082-5.082z"
+            style={{ stroke: 'rgb(51, 51, 51)', fill: 'rgb(51, 51, 51)', animationPlayState: 'paused' }}
+          />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default Logo;
