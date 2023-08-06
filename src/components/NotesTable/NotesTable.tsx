7f7f3ae283ef4141b4952cd5e9194fbc9468bd66
Daniil Popov <x6uhrox@gmail.com>
2023-08-06 14:42:44 +0300

    ADDED: archivedNote, removeNote

--- src/components/NotesTable/NotesTable.tsx
+++ src/components/NotesTable/NotesTable.tsx
@@ -1,21 +1,24 @@
-import {useEffect, useState} from 'react';
-import {Table, Button} from 'react-bootstrap';
-import TableRow from '../TableRow/TableRow';
-import TableModal from '../TableModal/TableModal';
-import { useAppSelector } from '../../hooks/hooks';
-import { selectNotes, selectCategoties } from '../../redux/notes/notesSelectors';
-import { NoteItem, HandleFunction, ModalType } from '../../types/types';
+import { useEffect, useState } from "react";
+import { Table, Button } from "react-bootstrap";
+import TableRow from "../TableRow/TableRow";
+import TableModal from "../TableModal/TableModal";
+import { useAppSelector } from "../../hooks/hooks";
+import {
+  selectNotes,
+  selectCategoties,
+} from "../../redux/notes/notesSelectors";
+import { NoteItem, HandleFunction, ModalType } from "../../types/types";
 
-const NotesTable = ({stats}:{stats:boolean}) => {
-
+const NotesTable = ({ stats }: { stats: boolean }) => {
   const [show, setShow] = useState(false);
   const [modalType, setModalType] = useState<ModalType>("add");
   const [selectedNote, setSelectedNote] = useState<NoteItem | null>(null);
+  const [archived, setArchived] = useState<boolean>(false);
 
   const handleClose: HandleFunction = () => {
     setShow(false);
     setSelectedNote(null);
-  }
+  };
   const handleShow: HandleFunction = (type, note) => {
     setShow(true);
     if (type) {
@@ -27,7 +30,7 @@ const NotesTable = ({stats}:{stats:boolean}) => {
   };
 
   const notes: NoteItem[] = useAppSelector(selectNotes);
-  const categories: string[] = useAppSelector(selectCategoties)
+  const categories: string[] = useAppSelector(selectCategoties);
 
   const plug = {
     id: "",
@@ -36,47 +39,93 @@ const NotesTable = ({stats}:{stats:boolean}) => {
     category: "",
     content: "",
     archive: false,
-  }
+  };
 
-  useEffect(() => {}, [notes]);  
+  useEffect(() => {}, [notes]);
 
   return (
     <>
-    <Table striped bordered hover>
-    <thead>
-      <tr className="table-primary">
-        {stats ? (
-          <>
-            <th scope="col">Name</th>
-            <th scope="col">Active</th>
-            <th scope="col">Archived</th>
-          </>
-        ) : (
-          <>
-            <th scope="col">Name</th>
-            <th scope="col">Created</th>
-            <th scope="col">Category</th>
-            <th scope="col">Content</th>
-            <th scope="col">Dates</th>
-            <th scope="col">Functions</th>
-          </>
+      <Table striped bordered hover>
+        <thead>
+          <tr className="table-primary">
+            {stats ? (
+              <>
+                <th scope="col">Name</th>
+                <th scope="col">Active</th>
+                <th scope="col">Archived</th>
+              </>
+            ) : (
+              <>
+                <th scope="col">Name</th>
+                <th scope="col">Created</th>
+                <th scope="col">Category</th>
+                <th scope="col">Content</th>
+                <th scope="col">Dates</th>
+                <th scope="col">Functions</th>
+              </>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {stats
+            ? categories.map((category) => (
+                <TableRow
+                  note={plug}
+                  notes={notes}
+                  stats={stats}
+                  category={category}
+                  key={category}
+                  handleShow={handleShow}
+                />
+              ))
+            : notes
+                .filter((note) => note.archive === archived)
+                .map((note) => (
+                  <TableRow
+                    note={note}
+                    notes={notes}
+                    stats={stats}
+                    category={""}
+                    key={note.id}
+                    handleShow={handleShow}
+                  />
+                ))}
+        </tbody>
+      </Table>
+      <div className="mb-3">
+        {!stats && (
+          <Button
+            className="btn primary-btn me-2"
+            onClick={() => handleShow("add")}
+          >
+            Create Note
+          </Button>
         )}
-      </tr>
-    </thead>
-    <tbody>
-      {stats
-        ? categories.map((category) => (
-            <TableRow  note ={plug} notes ={notes} stats={stats} category={category} key={category} handleShow={handleShow}/>
-          ))
-        : notes.map((note) => (
-            <TableRow  note ={note} notes ={[]} stats={stats} category={''} key={note.id} handleShow={handleShow}/>
-          ))}
-    </tbody>
-  </Table>
-  {!stats && <Button className='mb-3' onClick={() => handleShow('add')}>Create Note</Button>}
-  <TableModal show={show} handleClose={handleClose} modalType={modalType} initialNote={selectedNote}/>
-  </>
+        {!stats && (
+          <Button
+            className="btn primary-btn me-2"
+            onClick={() => setArchived(true)}
+          >
+            Archived Notes
+          </Button>
+        )}
+        {!stats && (
+          <Button
+            className="btn primary-btn"
+            onClick={() => setArchived(false)}
+          >
+            Active Notes
+          </Button>
+        )}
+      </div>
+      <TableModal
+        show={show}
+        handleClose={handleClose}
+        modalType={modalType}
+        initialNote={selectedNote}
+      />
+    </>
   );
-}
+};
 
 export default NotesTable;
--- src/components/TableRow/TableRow.tsx
+++ src/components/TableRow/TableRow.tsx
@@ -1,6 +1,8 @@
 import { NoteItem, HandleFunction } from "../../types/types";
 import { BiArchiveIn, BiEditAlt } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
+import { useAppDispatch } from "../../hooks/hooks";
+import { editNote, removeNote } from "../../redux/notes/notesSlice";
 import { truncateString } from "../../helpers/truncateString";
 import { countCategory } from "../../helpers/countCategory";
 
@@ -17,6 +19,18 @@ const TableRow = ({
   category: string;
   handleShow: HandleFunction;
 }) => {
+
+  const dispatch = useAppDispatch();
+
+  const handleClickArchived = () => {
+    const updateNote = { ...note, archive: !note.archive };
+    dispatch(editNote(updateNote));
+  };
+
+  const handleClickRemove = () => {
+    dispatch(removeNote(note));
+  }
+
   return (
     <tr className="table-light">
       {stats ? (
@@ -34,13 +48,23 @@ const TableRow = ({
           <td></td>
           <td>
             <div>
-              <button type="button" className="btn btn-primary me-2" name="edit" onClick={()=> handleShow('edit', note)}>
+              <button
+                type="button"
+                className="btn btn-primary me-2"
+                name="edit"
+                onClick={() => handleShow("edit", note)}
+              >
                 <BiEditAlt size={24} />
               </button>
-              <button type="button" className="btn btn-primary me-2" name="archive">
+              <button
+                type="button"
+                className="btn btn-primary me-2"
+                name="archive"
+                onClick={handleClickArchived}
+              >
                 <BiArchiveIn size={24} />
               </button>
-              <button type="button" className="btn btn-primary" name="remove">
+              <button type="button" className="btn btn-primary" name="remove" onClick={handleClickRemove}>
                 <AiOutlineDelete size={24} />
               </button>
             </div>
